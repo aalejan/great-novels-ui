f809499d358d74a1b88aea4cd701bad49835aa8e
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-01-06 11:09:40 -0500

    added all my different states I will use in Novels.jsx

---
+++ client.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import React from 'react'
+import Novels from './models/novels'
+
+render(
+  <Novels />,
+  document.getElementById('root'),
+)
--- components/Novels.jsx
+++ components/Novels.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Novel from './Novel'
+import Search from './Search'
 
 export default () => {
-    
+    const [searchTerm, setSearchTerm] = useState('')
+    const [novelsList, setNovelsList] = useState([])
+    const [filteredNovelsList, setFilteredNovelsList] = useState([])
 }
